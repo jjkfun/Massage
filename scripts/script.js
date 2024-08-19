dc6a28d5fe2dffdbccaf31112d337d9458e6a1bd
Jogoat <156339237+jjkfun@users.noreply.github.com>
2024-08-19 10:08:59 +0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+// const titles = document.querySelectorAll("accordion__title");
+// const contents = document.querySelectorAll("accordion__content");
+
+// titles.forEach((item) =>
+//   item.addEventListener("click", () => {
+//     if (activeContent.classList.contains("active")) {
+//       activeContent.classList.remove("active");
+//       item.classList.remove("active");
+//       activeContent.style.maxHeight = 0;
+//     } else {
+//       contents.forEach((element) => {
+//         element.classList.remove("active");
+//         element.style.maxHeight = 0;
+//       });
+
+//       titles.forEach(element = element.classList.remove('active'));
+
+
+//       item.classList.add('active');
+//       activeContent.classList.add('active');
+//       activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
+//     }
+//   })
+// );
